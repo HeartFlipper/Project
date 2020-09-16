9b282138923d03c5beb81dfb89ba58ed607eede6
anuragB <52213710+lastnameis-borah@users.noreply.github.com>
2020-09-16 20:57:00 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,15 @@
+superset=[1,2,3,4,5,6,7,8,9]
+print(superset[0:5:2])                           #[start:end:step]
+reverse=superset[-1::-1] 
+print(reverse)
+
+list=["sst","lst","shale"]
+x=list.count("sst")
+print(x)
+list.append("mud")          #add "mud" to the last of the list
+print(list)
+list.reverse()
+print(list)
+list.sort()                 #sort in ascending or alphabetical order
+print(list)
+print(len(list))            #length of the list
